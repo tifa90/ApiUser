c6ebb7668396784c520d49f9542baae66a760314
Latifa <latifa.akpo@yahoo.fr>
2023-01-02 22:55:19 +0100

    Modification du fichier de configuration de la base et de application.properties

--- src/main/java/com/user/userapi/conf/UserConf.java
+++ src/main/java/com/user/userapi/conf/UserConf.java
@@ -1,5 +1,6 @@
 package com.user.userapi.conf;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -15,10 +16,15 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -33,6 +39,75 @@ public class UserConf {
 	
 	@Autowired
 	private Environment env;
+	
+	@Bean
+    @Primary
+	public LocalContainerEntityManagerFactoryBean h2dbEntityManager() {
+		
+		  LocalContainerEntityManagerFactoryBean em
+          = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(bd1DataSource());
+        em.setPackagesToScan(
+          new String[] { "com.user.userapi.model", "com.user.userapi.dao" });
+        
+        HibernateJpaVendorAdapter vendorAdapter
+        = new HibernateJpaVendorAdapter();
+      em.setJpaVendorAdapter(vendorAdapter);
+      HashMap<String, Object> properties = new HashMap<>();
+      //properties.put("spring.jpa.hibernate.ddl-auto",
+        //env.getProperty("spring.jpa.hibernate.ddl-auto", "update"));
+      properties.put("hibernate.hbm2ddl.auto", "update");
+      properties.put("spring.jpa.show-sql",
+    	        env.getProperty("spring.jpa.show-sql", "true"));
+      properties.put("spring.jpa.database-platform",
+        env.getProperty("spring.jpa.database-platform"));
+      em.setJpaPropertyMap(properties);
+
+      return em;
+		
+		
+	}
+	
+	@Primary
+	@Bean(name="dataSource")
+	public DataSource bd1DataSource() {
+		 DriverManagerDataSource dataSource
+         = new DriverManagerDataSource();
+       dataSource.setDriverClassName(
+         env.getProperty("spring.datasource.driver-class-name"));
+       dataSource.setUrl(env.getProperty("spring.datasource.url"));
+       dataSource.setUsername(env.getProperty("spring.datasource.username"));
+       dataSource.setPassword(env.getProperty("spring.datasource.password"));
+
+       return dataSource;
+		
+		
+	}
+	
+	@Primary
+	@Bean(name="h2dbtr")
+	public PlatformTransactionManager h2dbtr() {
+		
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(h2dbEntityManager().getObject());
+		
+		return transactionManager;
+		
+		
+	}
+	
+	/*@EventListener
+	private void onApplication(ContextRefreshedEvent event) throws IOException{
+		ResourceDatabasePopulator dataPopulator = new ResourceDatabasePopulator(new ClassPathResource("insert.sql"));
+		dataPopulator.setContinueOnError(true);
+		dataPopulator.execute( bd1DataSource());
+		
+		
+		
+	}*/
+	
+	//@Autowired
+/*	private Environment env;
 	
 @Bean("datasource")
 //@ConfigurationProperties(prefix = "spring.datasource")
@@ -79,5 +154,5 @@ private Map hibernateProperties() {
 	return properties.entrySet().stream().collect(Collectors.toMap(e -> e.getKey().toString(), e -> e.getValue()));
 }
 
-
+*/
 }
--- src/main/resources/insert.sql
+++ src/main/resources/insert.sql
@@ -1,13 +1,13 @@
-/*
+
 INSERT INTO Utilisateur (IDUser, UserName, Birthdate, Country, Phone, Gender) VALUES (1, 'Martin', '2020-02-03', 'France','', '');
 
 INSERT INTO Utilisateur (IDUser, UserName, Birthdate, Country, Phone, Gender) VALUES (2, 'Bernard', '1979-12-31', 'France', '+33 6 78 69 20 34', 'Masculin');
 
 INSERT INTO Utilisateur (IDUser, UserName, Birthdate, Country, Phone, Gender) VALUES (3, 'Sara', '1988-06-15', 'Allemagne', '+49 170 765 4321', 'Feminin');
 
-INSERT INTO Utilisateur (IDUser, UserName, Birthdate, Country, Phone, Gender) VALUES (4, 'Hendryx', '1979-04-09', 'Royaume-Uni', '+44 79 87 65 43 21', 'Masculin');
+/*INSERT INTO Utilisateur (IDUser, UserName, Birthdate, Country, Phone, Gender) VALUES (4, 'Hendryx', '1979-04-09', 'Royaume-Uni', '+44 79 87 65 43 21', 'Masculin');
 
-INSERT INTO Utilisateur (IDUser, UserName, Birthdate, Country, Phone, Gender) VALUES (5, 'Maurice', '1990-08-18', 'France', '+33 7 56 34 76 80', 'Masculin');
+INSERT INTO Utilisateur (IDUser, UserName, Birthdate, Country, Phone, Gender) VALUES (5, 'Maurice', '1990-08-18', 'France', '+33 7 56 34 76 80', 'Masculin');*/
 
 */
 /*DROP TABLE IF EXISTS Utilisateur;  
